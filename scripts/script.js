c737d1cacb30d350a407018e5461d8b37e38d979
Megan <mlc025@ucsd.edu>
2021-02-12 14:05:19 -0800

    part 1-4

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,41 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+// local storage object
+localStorage = window.localStorage;
+
+window.addEventListener('DOMContentLoaded', async () => {
+  // if product array does not exist in local storage, fetch it
+  if (localStorage.getItem('prodArr') == null) {
+    // fetch product items
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    localStorage.setItem('prodArr', JSON.stringify(data)); // add array to local storage
+  }
+  // change string array back to json obj
+  var products = JSON.parse(localStorage.getItem('prodArr'));
+  // get the product-list-container element
+  var productList = document.getElementById('product-list');
+  // loop through products
+  for (var elem of products) {
+    console.log(elem);
+    // make product-item
+    var prod = document.createElement('product-item');
+
+    // set attributes according to json obj from array
+    prod.imageSrc = elem.image;
+    prod.imageAlt = elem.title;
+    prod.setTitle = elem.title;
+    prod.setPrice = elem.price;
+
+    // append new product to product-list-container
+    productList.appendChild(prod);
+  }
+
+  
+  // increment cart count
+  var count = parseInt(document.getElementById('cart-count').innerHTML);
+  count = count + 1;
 });
+
+
+
